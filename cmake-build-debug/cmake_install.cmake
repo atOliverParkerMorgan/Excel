8efb7102b7875124878cade8f9a7f3a9b46bc615
Oliver Morgan <morgaoli@cvut.cz>
2024-02-21 22:40:57 +0100

    Adding .idea and cmake-build-debug to .gitignore

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/olixandr/CLionProjects/Pa2
+# Install script for directory: /home/olixandr/Desktop/CLionProjects/Pa2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/olixandr/CLionProjects/Pa2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/olixandr/Desktop/CLionProjects/Pa2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
